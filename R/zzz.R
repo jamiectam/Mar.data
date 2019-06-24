d45d3492a3bf7a8051266c16b651b61fd4cafb28
McMahon <McMahonM@dfo-mpo.ca>
2019-06-24 13:50:23 -0300

    adds version check

---
+++ R/zzz.R
@@ -0,0 +1,9 @@
+# .onAttach <- function(libname, pkgname) {
+#   localVer = utils::packageDescription('Mar.data')$Version
+#   packageStartupMessage(paste0("Version: ", localVer))
+# }
+
+.onLoad <- function(libname, pkgname){
+  options(stringsAsFactors = FALSE)
+  Mar.utils::updateCheck(gitPkg = 'Maritimes/Mar.data')
+}
