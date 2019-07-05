f4a23949906a4835279776b48d77ccacf90a2ed1
McMahon <McMahonM@dfo-mpo.ca>
2019-07-05 14:36:06 -0300

    adds lo res coast

---
+++ R/coast_lores.R
@@ -0,0 +1,16 @@
+#' coast_lores Metadata
+#'
+#' This is simply a low resolution generated from some other packages that is
+#' generally convenient.  It was created on July 5, 2019 via:
+#' coastline =  maps::map(database = "world", regions = c("Canada", "USA", "France", "Greenland"),
+#'   						fill = T, resolution = 0)
+#' coast_lores = maptools::map2SpatialPolygons(coastline,
+#'                                              IDs = coastline$names,
+#'                                              proj4string = sp::CRS("+init=epsg:4326"))
+#' save(coast_lores,file = "coast_lores.rda")
+#' 
+#' @format  SpatialPolygons object
+#' \describe{
+#' \item{PUID}{polygon identifier}
+#' }
+"coast_lores"
