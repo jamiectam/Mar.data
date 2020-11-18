e762e57c7b15713cee186cf34435c792dd647208
McMahon <Mike.McMahon@dfo-mpo.gc.ca>
2020-11-18 10:25:25 -0400

    adds many new (sf) strata areas

---
+++ R/LobsterFAs_sf.R
@@ -0,0 +1,11 @@
+#' LobsterFAs_sf Metadata
+#'
+#' These are the lobster fishing areas converted to an sf object.
+#' This data was provided by the lobster unit on Nov 17, 2020
+#' in pbsmapping format.
+#'
+#' @format sf polygon
+#' \describe{
+#' \item{LFA}{Lobster Fishing Area}
+#' }
+"LobsterFAs_sf"
