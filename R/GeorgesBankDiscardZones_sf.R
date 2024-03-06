066f6f3a1e618dda159477713508def5f38d01f7
MMMcMahon <Mike.McMahon@dfo-mpo.gc.ca>
2024-03-06 13:56:01 +0000

    corrects documentation

--- R/GeorgesBankDiscardZones_sf.R
+++ R/GeorgesBankDiscardZones_sf.R
@@ -5,7 +5,7 @@
 #' composition in Gavaris, Van Eeckhaute, and Clark (2007).
 #' @source https://science-catalogue.canada.ca/record=4039145~S6
 #' @docType data
-#' @format SpatialPolygonsDataFrame frame with columns
+#' @format sf polygon
 #' \describe{
 #' \item{Id}{ESRI ArcMap Field - a number corresponding to the letter code in the Zone field (e.g. 1 for "Zone A", 2 for "Zone B", etc.)}
 #' \item{Zone}{Discard Zone - the word "Zone" plus an alphabetical identifier (e.g. "Zone A")}
